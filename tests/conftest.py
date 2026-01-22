5f054fa66eabd6313cc4f90d8a73ff0784273142
stnatter <stnatter@me.com>
2026-01-22 13:05:09 +0800

    Remove custom event_loop fixture to let pytest-asyncio handle it

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,6 @@ import ib_insync as ibi
 
 
 @pytest.fixture(scope='session')
-def event_loop():
-    loop = ibi.util.getLoop()
-    yield loop
-    loop.close()
-
-
-@pytest.mark.asyncio
-@pytest.fixture(scope='session')
 async def ib():
     ib = ibi.IB()
     await ib.connectAsync()
